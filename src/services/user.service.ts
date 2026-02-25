8bf09b42f5cde55e6d572f11b324c394392f58ce
Gabrel Freire <gfreirefumes@gmail.com>
2026-02-24 22:40:28 -0300

    🐛 Fix user endpoints

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,12 +3,13 @@ import api from "./api";
 
 export const userService = {
   getProfile: async () => {
-    const response = await api.get(`/users/me`);
+    const response = await api.get(`/user/profile`);
+    console.log(response.data);
     return response.data as UserProfile;
   },
 
   updateProfile: async (data: UpdateProfileData) => {
-    const response = await api.put(`/users/me`, data);
+    const response = await api.put(`/user/profile`, data);
     return response.data as UserProfile;
   },
 };
